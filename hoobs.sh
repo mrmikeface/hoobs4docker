896c81610ae1af429e22366a53df0394b859cfe0
Mike 👾 <7584016+MikeKnowles@users.noreply.github.com>
2021-12-24 22:28:21 +0000

    Adding fixes for hoobs 4.1.20

--- hoobs.sh
+++ hoobs.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e
 EXIT_CODE=0
-USER=root /usr/bin/hbs --verbose --debug --container install -p $PORT ||  EXIT_CODE=$?
-echo $EXIT_CODE
+USER=root /usr/bin/hbs --verbose --debug install -p $PORT ||  EXIT_CODE=$?
+echo "Install error code: ${EXIT_CODE}"
+echo "HOOBS cli version $(/usr/bin/hbs --version)"
 
 # same as systemctl --enable hoobsd
 mkdir -p /etc/systemd/system/multi-user.target.wants
-ln -s /etc/systemd/system/hoobsd.service /etc/systemd/system/multi-user.target.wants/hoobsd.service
+ln -sf /etc/systemd/system/hoobsd.service /etc/systemd/system/multi-user.target.wants/hoobsd.service
 
 # exec does not fork to a child pid so we're #1!
 exec /lib/systemd/systemd
